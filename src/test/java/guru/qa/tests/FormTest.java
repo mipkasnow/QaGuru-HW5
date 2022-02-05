d5d9849a04f004ec4089e59096fdaec232b11c00
mipkasnow <steamer08@mail.ru>
2022-02-05 17:07:30 +0300

    commit for coop

--- src/test/java/guru/qa/tests/FormTest.java
+++ src/test/java/guru/qa/tests/FormTest.java
@@ -35,4 +35,6 @@ public class FormTest extends BaseTest{
                 address, firstName + " " + lastName, email, phone, gender, stateAndCity));
     }
 
+    // Комментарий для PR Александр
+
 }
